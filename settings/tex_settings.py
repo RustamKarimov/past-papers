9c5f83ba61f8ec1df12523e46ae3ddf9ac7856d7
rustamkarimov <rustemk1981@gmail.com>
2023-08-22 16:45:16 +0200

    Stage and tex settings has been changed. File Structure for Physics past papers 2022 has been added.

---
+++ settings/tex_settings.py
@@ -0,0 +1,15 @@
+from manim import *
+
+
+my_template = TexTemplate()
+# my_template.add_to_preamble("\setlength{\textW}{\widthof{\alphaber} * \real{3}}")
+my_template.add_to_preamble(r"\usepackage{amssymb}")
+
+enum_template = TexTemplate()
+enum_template.add_to_preamble(r"\usepackage{enumitem}")
+
+tick = MathTex("\\checkmark", tex_template=my_template)
+
+tex_environment = "\\begin{tabular}{p{18 cm}}{\\flushleft}"
+half_tex_environment = "\\begin{tabular}{p{8 cm}}{\\flushleft}"
+
