ecb1bc7ef23b4bb41f2396b261c796b70f68abbf
Rustam Karimov <rustemk1981@gmail.com>
2023-08-22 16:12:06 +0200

    File settings has been implemented

---
+++ settings/file_settings.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent.parent
+
+ASSETS_DIR = BASE_DIR / "Assets"
+IMAGES_DIR = ASSETS_DIR / "Images"
+
+BUTA_IMAGES = IMAGES_DIR / "Buta"
+LOGO = BUTA_IMAGES / "logo.svg"
