26bc2302bdc6e7dd2c928bd04ad4f9e2acf31848
Rustam Karimov <rustemk1981@gmail.com>
2023-08-22 15:48:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Main program responsible for generating past paper videos
+prepared by using Manim.
+"""
+
+from manim import *
+
+from subprocess import run
+
+if __name__ == "__main__":
+    pass
