2b299d9075f74f4b9987e0e9d35d6073f3c1efaa
Rustam Karimov <rustemk1981@gmail.com>
2023-08-22 16:18:16 +0200

    Main.py now runs manim command

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ Main program responsible for generating past paper videos
 prepared by using Manim.
 """
 
-from manim import *
-
 from subprocess import run
 
+from settings import file_settings as fs
+
+DISABLE = False
+
+SUBJECT = "Physics"
+YEAR = "2022"
+QUESTION = "1"
+SUB_QUESTION = "1"
+FILE = fs.BASE_DIR / SUBJECT / YEAR / f"Question_{QUESTION}" / f"question_{QUESTION}_{SUB_QUESTION}.py"
+SCENE = f"Question_{QUESTION}_{SUB_QUESTION}"
+QUALITY = "h"
+CUSTOM_FOLDERS = "--custom_folders"
+DISABLE_CACHING = "--disable_caching" if DISABLE else ""
+
 if __name__ == "__main__":
-    pass
+    run(["manim", FILE, SCENE, f"-pq{QUALITY}", CUSTOM_FOLDERS, DISABLE_CACHING])
