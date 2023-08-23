279b09a8b16d1cfb216d9b010e3397099d71e94e
rustamkarimov <rustemk1981@gmail.com>
2023-08-23 20:54:14 +0200

    definitions.py has been added to physics_packages

---
+++ physics_packages/definitions.py
@@ -0,0 +1,36 @@
+OR = "OR \\newline \\newline"
+
+SECOND_LAW_OF_MOTION_1 = """
+When a resultant/net force acts on an object, 
+the object will accelerate in the direction of the force. 
+The acceleration is directly proportional to the resultant/net force and 
+inversely proportional to the mass of the object. \\newline \\newline
+"""
+
+SECOND_LAW_OF_MOTION_2 = """
+The resultant/net force acting on an object is 
+equal to the rate of change of momentum of the object.  
+"""
+
+FREE_FALL_1 = """
+Motion under the influence of gravity/weight/gravitational force only. \\newline \\newline
+"""
+
+FREE_FALL_2 = """
+Motion in which the only force acting is gravity/weight/gravitational force.
+"""
+
+CONSERVATION_OF_LINEAR_MOMENTUM = """
+In an isolated/closed system the total (linear) momentum is
+conserved/remains constant. 
+"""
+
+NON_CONSERVATIVE_FORCE_1 = """
+A force is non-conservative if the work done by the force on an object 
+(which is moving between two points) depends on the path taken. \\newline \\newline
+"""
+
+NON_CONSERVATIVE_FORCE_2 = """
+A force is non-conservative if the work it does in moving an object around a
+closed path is non-zero.
+"""
