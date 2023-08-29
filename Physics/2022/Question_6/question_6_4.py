98cea686e3c5e2767f9972ef04de08905e1d08fe
Rustam Karimov <rustemk1981@gmail.com>
2023-08-29 11:55:04 +0200

    Question 6.4 completed

--- Physics/2022/Question_6/question_6_4.py
+++ Physics/2022/Question_6/question_6_4.py
@@ -9,6 +9,7 @@ from utilitites import animations as anim
 
 from settings.class_settings import QuestionScene
 from settings import stage_settings as ss
+from settings.tex_settings import tick
 
 
 class Question_6_4(QuestionScene):
@@ -25,7 +26,7 @@ class Question_6_4(QuestionScene):
 
         question_title = titles[0]
         question_kwargs = {"tex_environment": lu.get_tabular_environment(p_size=16)}
-        _ = get_the_question(self, question_title, self.question, **question_kwargs)
+        paragraphs = get_the_question(self, question_title, self.question, **question_kwargs)
 
         graph_box = boxes[1]
         graph_kwargs = {
@@ -61,22 +62,22 @@ class Question_6_4(QuestionScene):
         equations = MathTex(
             "Gradient = \\frac {\\Delta y} {\\Delta x}",
             "= \\frac {f_L} {f_s}",
-            "=1,06",
-            "f_L = \\frac {v \\pm v_L} {v \\pm v_s} f_s",
+            "= 1,06",
+            "f_L = \\frac {v \\pm v_L} {v \\pm v_s} \\times f_s",
             "\\frac {f_L} {f_s} = \\frac {v \\pm v_L} {v \\pm v_s}",
             "1,06 = \\frac {340 + v_L} {340}",
             "1,06 \\times 340 = 340 + v_L",
             "360,4 = 340 + v_L",
             "360,4 - 340 = v_L",
-            "v_L = 20,4 m \\times s^{-1}",
+            "v_L = 20,4 \\, m \\times s^{-1}",
             **eq_kwargs
         )
 
-        equations_group = VGroup(
-            equations[:2],
-            VGroup(equations[3], equations[4]).arrange(RIGHT * 2),
-            VGroup(equations[5], equations[6]).arrange(RIGHT * 2),
-            VGroup(equations[7], equations[8]).arrange(RIGHT * 2),
+        VGroup(
+            equations[:3],
+            VGroup(equations[3], equations[4]).arrange(RIGHT * 6),
+            VGroup(equations[5], equations[6]).arrange(RIGHT * 6),
+            VGroup(equations[7], equations[8]).arrange(RIGHT * 6),
             equations[9]
         ).arrange(DOWN).move_to(solution_box)
 
@@ -120,8 +121,107 @@ class Question_6_4(QuestionScene):
         self.wait()
 
         transform_matrix = [
-            [0, 1, 2, 3, 5, 6, 7, 8, 11, 20, 23, 24, 25, 26, 27],
-            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+            [8, 9, 10, 11, 12, 13],
+            [0, 1, 2, 3, 4, 5]
         ]
 
         anim.get_equation_transform(self, equations[0], equations[1], transform_matrix, run_time=2)
+
+        transform_matrix = [
+            [0, 1, 3, 4, 5],
+            [0, 1, 2, 3, 4]
+        ]
+
+        anim.get_equation_transform(self, equations[1], equations[2], transform_matrix, run_time=2)
+
+        self.play(
+            Indicate(equations[2]),
+            Indicate(paragraphs[0][-5:-1]),
+            run_time=2
+        )
+        self.wait()
+
+        self.play(FadeIn(equations[3]))
+        self.wait()
+
+        transform_matrix = [
+            [0, 1, -3, -2, -1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            list(range(15))
+        ]
+
+        anim.get_equation_transform(self, equations[3], equations[4], transform_matrix, run_time=2)
+
+        transform_matrix = [
+            [0, 2, 3, 4, 5, 6, 6, 6, 7, 8, 9, 10, 11, 12, 13],
+            list(range(15))
+        ]
+
+        anim.get_equation_transform(self, equations[4], equations[5], transform_matrix, run_time=2)
+
+        frame_1 = SurroundingRectangle(equations[:3], stroke_width=1)
+        frame_2 = SurroundingRectangle(equations[4][:5], stroke_width=1)
+        frame_3 = SurroundingRectangle(equations[5][:4], stroke_width=1)
+
+        self.play(
+            Create(frame_1), Create(frame_2), Create(frame_3),
+            rate_func=rate_functions.there_and_back,
+            run_time=5
+        )
+        self.wait()
+
+        self.play(
+            Indicate(equations[5][5:8]),
+            Indicate(equations[5][-3:]),
+            Indicate(paragraphs[1][24:32]),
+            run_time=3,
+        )
+        self.wait()
+
+        transform_matrix = [
+            [0, 1, 2, 3, -4, -3, -3, -1, 4, 5, 6, 7, 8, 9, 10],
+            list(range(15))
+        ]
+
+        anim.get_equation_transform(self, equations[5], equations[6], transform_matrix, run_time=2)
+
+        transform_matrix = [
+            [0, 0, 0, 1, 4, 8, 9, 10, 11, 12, 13, 14],
+            list(range(12))
+        ]
+
+        anim.get_equation_transform(self, equations[6], equations[7], transform_matrix, run_time=2)
+
+        transform_matrix = [
+            [0, 1, 2, 3, 4, 9, 6, 7, 8, 5, 10, 11],
+            list(range(12))
+        ]
+
+        anim.get_equation_transform(self, equations[7], equations[8], transform_matrix, run_time=2)
+
+        transform_matrix = [
+            [-2, -1, -3, 0, 2, 3, 4, 4, 5, 6, 7, 8],
+            list(range(13))
+        ]
+
+        anim.get_equation_transform(self, equations[8], equations[9], transform_matrix, run_time=2)
+
+        tick_1 = tick.copy()
+        tick_1.font_size = ss.MEDIUM_TEX_SIZE
+        tick_1.next_to(equations[3], RIGHT, buff=0.2)
+
+        tick_2 = tick_1.copy()
+        tick_2.next_to(equations[5], LEFT, buff=0.1)
+        tick_3 = tick_1.copy()
+        tick_3.next_to(tick_2, LEFT, buff=0.05)
+
+        tick_4 = tick_1.copy()
+        tick_4.next_to(equations[5], RIGHT, buff=0.2)
+
+        tick_5 = tick_1.copy()
+        tick_5.next_to(equations[9], RIGHT, buff=0.2)
+
+        ticks = [tick_1, tick_2, tick_3, tick_4, tick_5]
+
+        for a_tick in ticks:
+            self.play(Write(a_tick))
+            self.wait()
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ QUESTION = "6"
 SUB_QUESTION = "4"
 FILE = fs.BASE_DIR / SUBJECT / YEAR / f"Question_{QUESTION}" / f"question_{QUESTION}_{SUB_QUESTION}.py"
 SCENE = f"Question_{QUESTION}_{SUB_QUESTION}"
-QUALITY = "l"
+QUALITY = "h"
 CUSTOM_FOLDERS = "--custom_folders"
 DISABLE_CACHING = "--disable_caching" if DISABLE else ""
 
