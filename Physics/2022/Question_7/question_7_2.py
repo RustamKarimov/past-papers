06f128a49958c65bf9b601c378fc5ac6263583fa
Rustam Karimov <rustemk1981@gmail.com>
2023-08-31 16:37:05 +0200

    Trying to implement data classes

---
+++ Physics/2022/Question_7/question_7_2.py
@@ -0,0 +1,34 @@
+from manim import *
+
+from utilitites import stage_setup as stage
+from utilitites.box_alignments import get_box_details
+from utilitites.latex_utilities import get_the_question, display_paragraphs
+from utilitites import latex_utilities as lu
+
+from physics_packages.definitions import COULOMBS_LAW
+
+from settings.class_settings import QuestionScene
+from settings import stage_settings as ss
+from settings.tex_settings import tick
+
+
+class Question_7_2(QuestionScene):
+    def construct(self):
+        titles = ("Question:", "Diagram:", "Solution:")
+        box_sizes = ((0.4, 1), (0.58, 0.48), (0.58, 0.48))
+        box_details = get_box_details(box_structure="crr", titles=titles, box_sizes=box_sizes)
+
+        titles, boxes = stage.set_the_stage(
+            scene=self,
+            title=self.get_the_title(),
+            box_details=box_details,
+        )
+
+        question_title = titles[0]
+        question_kwargs = {
+            "tex_environment": lu.get_tabular_environment(p_size=6),
+            "font_size": ss.MEDIUM_TEX_SIZE
+        }
+        _ = get_the_question(self, question_title, self.question, **question_kwargs)
+
+
