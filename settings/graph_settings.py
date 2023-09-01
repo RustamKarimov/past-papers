30f733fea21351edf161cf0c8987de1959f7efe4
Rustam Karimov <rustemk1981@gmail.com>
2023-09-01 15:24:24 +0200

    dataclasses implemented but not used yet instead of dictionaries in the graph_settings.py

--- settings/graph_settings.py
+++ settings/graph_settings.py
@@ -2,6 +2,7 @@ from manim import *
 
 from collections.abc import Sequence, Callable
 from dataclasses import dataclass, field, asdict
+from numpy import ndarray
 
 from settings import stage_settings as ss
 
@@ -38,22 +39,51 @@ Y_LABEL_SETTINGS = {
     "y_label_rotation": 0,
 }
 
-PLOT_SETTINGS = {
-    "x_range": [0, 0.8],
-    "y_range": [0, 0.8],
-    "function": lambda x: x,
-}
 
-
-def default_function():
+def direct_proportion_function():
     return lambda x: x
 
 
 @dataclass
 class PlotSettings:
     x_range: Sequence[float] = field(default_factory=lambda: (0, 0.8))
-    function: Callable = field(default_factory=default_function)
+    function: Callable = field(default_factory=direct_proportion_function)
 
+    def as_dict(self):
+        return asdict(self)
+
+
+@dataclass
+class AxisLabelSettings:
+    axis: str = "x"
+    label: str = "x"
+    label_edge: ndarray = field(default_factory=lambda: UR)
+    label_direction: ndarray = field(default_factory=lambda: UR)
+    buff: int = 0.1
+    font_size: int = ss.SMALL_TEX_SIZE
+    angle: int = 0
+
+
+@dataclass
+class AxisConfig:
+    include_numbers: bool = False
+    include_ticks: bool = False
+    tip_width: float = 0.1
+    tip_height: float = 0.1
+
+    def as_dict(self):
+        return asdict(self)
+
+
+@dataclass
+class GraphSettings:
+    x_range: Sequence[float] = field(default_factory=lambda: (0, 1))
+    y_range: Sequence[float] = field(default_factory=lambda: (0, 1))
+    x_length: int = 2
+    y_length: int = 2
+    axis_config: AxisConfig = field(default_factory=AxisConfig)
+    x_axis_config: AxisLabelSettings = field(default_factory=AxisLabelSettings)
+    y_axis_config: AxisLabelSettings = field(default_factory=AxisLabelSettings)
 
     def as_dict(self):
         return asdict(self)
