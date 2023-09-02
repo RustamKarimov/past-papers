c4c5ed2c8bebcb46d8110617b05c81c33d97db39
rustamkarimov <rustemk1981@gmail.com>
2023-09-02 14:59:40 +0200

    documentation has been added to graph_settings.py and graph_utilities.py

--- settings/graph_settings.py
+++ settings/graph_settings.py
@@ -13,15 +13,34 @@ def direct_proportion_function():
 
 @dataclass
 class PlotSettings:
+    """
+    A Dataclass consisting of x_range and function fields which are required to plot a graph.
+    x_range: range for which graph will be plotted.
+    function: function used to plot the graph
+    """
     x_range: Sequence[float] = field(default_factory=lambda: (0, 0.8))
     function: Callable = field(default_factory=direct_proportion_function)
 
     def as_dict(self):
+        """
+        Converts the fields of the class to a dictionary
+        :return: dictionary version of the class
+        """
         return asdict(self)
 
 
 @dataclass
 class AxisLabelSettings:
+    """
+    A dataclass consisting of fields required for the labeling an axis of a graph.
+    axis: axis to tbe labeled (by default it is "x" axis)
+    label: The label of the given axis
+    edge: Position of the label due to the axis
+    direction: Direction of the label due to the axis
+    buff: Distance of the label from the axis
+    font_size: The font size of the label
+    angle: An angle of rotation for the label
+    """
     axis: str = "x"
     label: str = "x"
     edge: ndarray = field(default_factory=lambda: UR)
@@ -33,17 +52,36 @@ class AxisLabelSettings:
 
 @dataclass
 class AxisConfig:
+    """
+    Axis configuration settings for the graph.
+    include_numbers: If numbers will be displayed or not (by default they are not displayed)
+    include_numbers: If ticks will be displayed or not (by default they are not displayed)
+    tip_width: width of the tip of the axis (by default it's 0.1)
+    tip_height: height of the tip of the axis (by default it's 0.1)
+    """
     include_numbers: bool = False
     include_ticks: bool = False
     tip_width: float = 0.1
     tip_height: float = 0.1
 
     def as_dict(self):
+        """
+        Converts the fields of the class to a dictionary
+        :return: dictionary version of the class
+        """
         return asdict(self)
 
 
 @dataclass
 class AxesSettings:
+    """
+    A dataclass consisting of settings to draw an axes for a graph.
+    x_range: range of x values to be represented on the graph
+    y_range: range of y values to be represented on the graph
+    x_length: length of x axis
+    y_length: length of y axis
+    axis_config: Configurations for both axes
+    """
     x_range: Sequence[float] = field(default_factory=lambda: (0, 1))
     y_range: Sequence[float] = field(default_factory=lambda: (0, 1))
     x_length: int = 2
@@ -51,11 +89,22 @@ class AxesSettings:
     axis_config: AxisConfig = field(default_factory=AxisConfig)
 
     def as_dict(self):
+        """
+        Converts the fields of the class to a dictionary
+        :return: dictionary version of the class
+        """
         return asdict(self)
 
 
 @dataclass
 class GraphSettings:
+    """
+    A dataclass consisting of settings for various parts of a graph.
+    axes_settings: settings for axes
+    x_label_settings: settings for the label of x axis
+    y_label_settings: settings for the label of y axis
+    plot_settings: settings of the plot on the graph
+    """
     axes_settings: AxesSettings = field(default_factory=AxesSettings)
     x_label_settings: AxisLabelSettings = field(default_factory=AxisLabelSettings)
     y_label_settings: AxisLabelSettings = field(default_factory=AxisLabelSettings)
@@ -64,6 +113,13 @@ class GraphSettings:
 
 @dataclass
 class GraphParts:
+    """
+    A dataclass to keep record of the various parts of the graph.
+    axes: an Axes object
+    x_label: Label generated for the label of x axis on the graph
+    y_label: Label generated for the label of y axis on the graph
+    plot: the plot mobject
+    """
     axes: Axes
     x_label: Mobject
     y_label: Mobject
--- utilitites/graph_utilities.py
+++ utilitites/graph_utilities.py
@@ -3,7 +3,13 @@ from manim import *
 from settings import graph_settings as gs
 
 
-def generate_axis_label(axes: Axes, label_settings:gs.AxisLabelSettings):
+def generate_axis_label(axes: Axes, label_settings: gs.AxisLabelSettings) -> Mobject:
+    """
+    Generates label for the specified axis.
+    :param axes: an Axes object for which the labels are generated
+    :param label_settings: label settings of the required axis
+    :return: a mobject containing the label of the axis
+    """
 
     get_label = getattr(axes, f"get_{label_settings.axis}_axis_label")
 
@@ -15,7 +21,7 @@ def generate_axis_label(axes: Axes, label_settings:gs.AxisLabelSettings):
     ).rotate(label_settings.angle)
 
 
-def generate_plot(plot_settings: gs.PlotSettings):
+def generate_plot(plot_settings: gs.PlotSettings) -> dict:
     """
     Generates the parameters for the plot function by merging the default plot settings and the settings
     provided by the user.
@@ -28,7 +34,12 @@ def generate_plot(plot_settings: gs.PlotSettings):
     }
 
 
-def generate_the_graph(graph_settings: gs.GraphSettings):
+def generate_the_graph(graph_settings: gs.GraphSettings) -> (VGroup, gs.GraphParts):
+    """
+    Generates a graph based on the provided graph settings
+    :param graph_settings: graph settings required to draw a graph.
+    :return: Vgroup mobject consisting of different parts of the graph and a dataclass keeping track of these parts
+    """
     axes_settings = graph_settings.axes_settings
     x_label_settings = graph_settings.x_label_settings
     y_label_settings = graph_settings.y_label_settings
